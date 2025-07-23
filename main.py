0dd218d5e10a4363f5ea837cffe0e43a6420c2f5
frankheinz87 <frankheinz1987@gmail.com>
2025-07-23 17:47:25 +0200

    implemented command line prompt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,7 +13,10 @@ client = genai.Client(api_key=api_key)
 #contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
 def main():
-    response = client.models.generate_content(model = "gemini-2.0-flash-001", contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    if len(sys.argv) < 2:
+        print("no prompt provided")
+        exit(1)
+    response = client.models.generate_content(model = "gemini-2.0-flash-001", contents = sys.argv[1])
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
