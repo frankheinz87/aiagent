74135c7357deaff6fc09224644f6011b6908aeca
frankheinz87 <frankheinz1987@gmail.com>
2025-07-24 18:09:31 +0200

    implemented verbose

--- main.py
+++ main.py
@@ -6,20 +6,24 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 from google import genai
+from google.genai import types
 
 client = genai.Client(api_key=api_key)
 
-#model = "gemini-2.0-flash-001"
-#contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
 
 def main():
     if len(sys.argv) < 2:
         print("no prompt provided")
         exit(1)
-    response = client.models.generate_content(model = "gemini-2.0-flash-001", contents = sys.argv[1])
+    messages = [types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),]
+    response = client.models.generate_content(model = "gemini-2.0-flash-001", contents = messages)
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if len(sys.argv) > 2:
+        if sys.argv[2] == "--verbose":
+            print(f"User prompt: {sys.argv[1]}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
