fa3eafddd43dd7312c714825260e6c3a8864728e
frankheinz87 <frankheinz1987@gmail.com>
2025-08-03 19:10:35 +0200

    implemented get_files_info into main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from dotenv import load_dotenv
+from functions.get_files_info import schema_get_files_info, available_functions
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -17,13 +18,25 @@ def main():
         print("no prompt provided")
         exit(1)
     messages = [types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),]
-    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+    system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
     response = client.models.generate_content(
         model = "gemini-2.0-flash-001", 
         contents = messages,
-        config = types.GenerateContentConfig(system_instruction = system_prompt))
+        config = types.GenerateContentConfig(tools = [available_functions], system_instruction = system_prompt))
     
-    print(response.text)
+    if response.function_calls != None:
+        for call in response.function_calls:
+            print(f"Calling function: {call.name}({call.args})")
+    else:
+        print(response.text)
     if len(sys.argv) > 2:
         if sys.argv[2] == "--verbose":
             print(f"User prompt: {sys.argv[1]}")
