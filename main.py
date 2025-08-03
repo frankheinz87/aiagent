eae9668da9ccd1109af19359c05390c2a3130f4b
frankheinz87 <frankheinz1987@gmail.com>
2025-08-03 18:49:04 +0200

    included system_prompt into main.py

--- main.py
+++ main.py
@@ -17,7 +17,12 @@ def main():
         print("no prompt provided")
         exit(1)
     messages = [types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),]
-    response = client.models.generate_content(model = "gemini-2.0-flash-001", contents = messages)
+    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+    response = client.models.generate_content(
+        model = "gemini-2.0-flash-001", 
+        contents = messages,
+        config = types.GenerateContentConfig(system_instruction = system_prompt))
+    
     print(response.text)
     if len(sys.argv) > 2:
         if sys.argv[2] == "--verbose":
